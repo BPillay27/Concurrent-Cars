ad2f378f052917d221c1d2a9a85692e9fb94115f
BPillay27 <byronpillay2704@gmail.com>
2025-06-26 10:42:52 +0200

    Update main.java

--- main.java
+++ main.java
@@ -1,1 +1,10 @@
+//Practice idea. Remote controlled car. Need to track who has access concurrently and modify data accordingly.
+
+import java.lang.Thread;
+
+public class Main {
+    public static void main(String[] args) {
+        
+    }
+}
 
