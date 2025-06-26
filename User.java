ed2ef0ce0ec3b2f7df738bbce9fd1eefc3fd3aa5
BPillay27 <byronpillay2704@gmail.com>
2025-06-26 10:40:26 +0200

    Add files via upload

---
+++ User.java
@@ -0,0 +1,53 @@
+public class User {
+    String name;
+    int id;
+    Car controlling;
+
+    User(String name, int id) {
+        this.name = name;
+        this.id = id;
+        controlling = null;
+    }
+
+    public boolean isControlling() {
+        return (controlling == null) ? false : true;
+    }
+
+    public boolean isControlling(Car x) {
+        if (controlling == null) {
+            return false;
+        } else {
+            if (x == null) {
+                return false;
+            } else {
+                // ATP: There is a controll, and there is a input
+                return (x.id == controlling.id) ? true : false;
+            }
+        }
+    }
+
+    public boolean updateSpeed(int x) {
+        if (controlling == null) {
+            return false;
+        }
+
+        if (x < 0) {
+            return controlling.slowDown();
+        } else {
+            return controlling.speedUp();
+        }
+    }
+
+    public boolean releaseCar() {
+        if (controlling == null) {
+            return true;
+        }
+
+        while (controlling.currentSpeed != 0) {
+            controlling.slowDown();
+        }
+
+        controlling = null;
+        return true;
+    }
+}
