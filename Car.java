0ce6fe146d50dde1a0175f224afbb8803fbed13e
BPillay27 <byronpillay2704@gmail.com>
2025-06-26 10:40:54 +0200

    Add files via upload

---
+++ Car.java
@@ -0,0 +1,34 @@
+public class Car {
+    public String brand;
+    public int mileage;
+    public String make;
+    public int id;
+    public int currentSpeed = 0;
+    public boolean available = true;
+
+    Car(String brand, int mileage, String make, int id) {
+        this.brand = brand;
+        this.mileage = mileage;
+        this.make = make;
+        this.id = id;
+    }
+
+    public void updateMileage(int x) {
+        mileage += x;
+    }
+
+    public boolean speedUp() {
+        currentSpeed += 10;
+        return true;
+    }
+
+    public boolean slowDown() {
+        if (currentSpeed <= 10) {
+            currentSpeed = 0;
+            return true;
+        } else {
+            currentSpeed -= 10;
+            return true;
+        }
+    }
+}
